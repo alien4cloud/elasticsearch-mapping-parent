54679a9551b9f8c55896384d7222a3a9c0186b11
vuminhkh <minh.khang.vu.mkv@gmail.com>
2014-02-25 11:58:13 +0100

    Close elastic search when context is disposed

--- elasticsearch-mapping/src/main/java/org/elasticsearch/mapping/ElasticSearchClient.java
+++ elasticsearch-mapping/src/main/java/org/elasticsearch/mapping/ElasticSearchClient.java
@@ -4,6 +4,8 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,53 +18,65 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ElasticSearchClient {
-	private Node node;
-	private boolean isClient;
-	private boolean isLocal;
-	private String clusterName;
-	private boolean resetData = false;
 
-	@PostConstruct
-	public void initialize() {
-		this.node = NodeBuilder.nodeBuilder().client(this.isClient).clusterName(this.clusterName).local(this.isLocal)
-				.node();
-		if (this.resetData) { // removes all indices from elastic search. For Integration testing only.
-			this.node.client().admin().indices().prepareDelete(new String[] {}).execute().actionGet();
-		}
-	}
+    private static final ESLogger LOGGER = Loggers.getLogger(MappingBuilder.class);
 
-	/**
-	 * Get the elastic search client.
-	 * 
-	 * @return The elastic search client.
-	 */
-	public Client getClient() {
-		return this.node.client();
-	}
+    private Node node;
+    private boolean isClient;
+    private boolean isLocal;
+    private String clusterName;
+    private boolean resetData = false;
 
-	@PreDestroy
-	public void tearDown() {
-		this.node.close();
-	}
+    @PostConstruct
+    public void initialize() {
+        this.node = NodeBuilder.nodeBuilder().client(this.isClient).clusterName(this.clusterName).local(this.isLocal)
+                .node();
+        if (this.resetData) { // removes all indices from elastic search. For Integration testing only.
+            this.node.client().admin().indices().prepareDelete(new String[] {}).execute().actionGet();
+        }
+        LOGGER.info("Initialized ElasticSearch client for cluster <" + this.clusterName + ">");
+    }
 
-	@Value("#{elasticsearchConfig['elasticSearch.clusterName']}")
-	public void setClusterName(final String clusterName) {
-		this.clusterName = clusterName;
-	}
+    @PreDestroy
+    public void close() {
+        if (node != null) {
+            node.close();
+        }
+        LOGGER.info("Closed ElasticSearch client for cluster <" + this.clusterName + ">");
+    }
 
-	@Value("#{elasticsearchConfig['elasticSearch.local']}")
-	public void setLocal(final boolean isLocal) {
-		this.isLocal = isLocal;
-	}
+    /**
+     * Get the elastic search client.
+     * 
+     * @return The elastic search client.
+     */
+    public Client getClient() {
+        return this.node.client();
+    }
 
-	@Value("#{elasticsearchConfig['elasticSearch.client']}")
-	public void setClient(final boolean isClient) {
-		this.isClient = isClient;
-	}
+    @PreDestroy
+    public void tearDown() {
+        this.node.close();
+    }
 
-	@Value("#{elasticsearchConfig['elasticSearch.resetData']}")
-	public void setResetData(final boolean resetData) {
-		this.resetData = resetData;
-	}
+    @Value("#{elasticsearchConfig['elasticSearch.clusterName']}")
+    public void setClusterName(final String clusterName) {
+        this.clusterName = clusterName;
+    }
+
+    @Value("#{elasticsearchConfig['elasticSearch.local']}")
+    public void setLocal(final boolean isLocal) {
+        this.isLocal = isLocal;
+    }
+
+    @Value("#{elasticsearchConfig['elasticSearch.client']}")
+    public void setClient(final boolean isClient) {
+        this.isClient = isClient;
+    }
+
+    @Value("#{elasticsearchConfig['elasticSearch.resetData']}")
+    public void setResetData(final boolean resetData) {
+        this.resetData = resetData;
+    }
 
 }
