6907290189ab257f967c50569e0e14e32ce2035e
Luc Boutier <luc@fastconnect.fr>
2016-06-13 17:57:46 +0200

    Fix error in dependency Fix java version and java version compliance in test.

--- elasticsearch-mapping/src/test/java/org/elasticsearch/mapping/MappingBuilderTest.java
+++ elasticsearch-mapping/src/test/java/org/elasticsearch/mapping/MappingBuilderTest.java
@@ -1,6 +1,8 @@
 package org.elasticsearch.mapping;
 
 import java.beans.IntrospectionException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,6 +14,8 @@ import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+
+import static java.awt.SystemColor.text;
 
 /**
  * Test the mappings.
@@ -25,7 +29,8 @@ public class MappingBuilderTest {
         MappingBuilder mappingBuilder = new MappingBuilder();
         mappingBuilder.initialize("org.elasticsearch.mapping.model");
         String personMapping = mappingBuilder.getMapping(Person.class);
-        String expected = Files.readAllLines(Paths.get("src/test/resources/mapping.json")).get(0);
+        BufferedReader brTest = new BufferedReader(new FileReader(Paths.get("src/test/resources/mapping.json").toFile()));
+        String expected = brTest .readLine();
         Assert.assertEquals(expected, personMapping);
     }
 }
